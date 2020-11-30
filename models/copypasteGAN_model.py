1dbd8210ad1afb0e662d9d4bb8bbb6cd64b1d127
Tom Lotze <tom.lotze@student.auc.nl>
2020-11-30 10:25:41 +0100

    create anti-shortcut and backward D

--- models/copypasteGAN_model.py
+++ models/copypasteGAN_model.py
@@ -38,7 +38,7 @@ class CopyPasteGANModel(BaseModel):
         # batch size of 80 per GPU is used (and 4 GPUs)
         # norm is instanc by default
 
-        parser.set_defaults(dataset_mode='single', output_nc=1, load_size=70, crop_size= 64,batch_size=80, lr=1e-4, lr_policy="step", n_epochs=1, netG="copy", netD="copy", dataroot="datasets")  # You can rewrite default values for this model. For example, this model usually uses aligned dataset as its dataset.
+        parser.set_defaults(dataset_mode='double', load_size=70, crop_size= 64,batch_size=80, lr=1e-4, lr_policy="step", n_epochs=1, netG="copy", netD="copy", dataroot="datasets")  # You can rewrite default values for this model. For example, this model usually uses aligned dataset as its dataset.
         if is_train:
             parser.add_argument('--lambda_aux', type=float, default=0.2, help='weight for the auxiliary mask loss')  # You can define new arguments for this model.
 
@@ -59,10 +59,10 @@ class CopyPasteGANModel(BaseModel):
         # specify the training losses you want to print out. The program will call base_model.get_current_losses to plot the losses to the console and save them to the disk.
         self.loss_names = ['loss_G', "loss_D", "loss_AUX"]
         # specify the images you want to save and display. The program will call base_model.get_current_visuals to save and display these images.
-        self.visual_names = ['batch', 'data_B', 'output']
+        self.visual_names = ['composite', 'grounded_fake']
 
-        # define generator
-        self.netG = networks.define_G(opt.input_nc, opt.output_nc, ngf=opt.ngf, netG=opt.netG, norm=opt.norm, gpu_ids=self.gpu_ids)
+        # define generator, output_nc is hardcoded to 1
+        self.netG = networks.define_G(opt.input_nc, 1, ngf=opt.ngf, netG=opt.netG, norm=opt.norm, gpu_ids=self.gpu_ids)
         # specify which models to save to disk
         self.model_names = ['G']
 
@@ -90,24 +90,32 @@ class CopyPasteGANModel(BaseModel):
         Parameters:
             input: a dictionary that contains the data itself and its metadata information.
         """
-        self.batch = input['A'].to(self.device)  # get image data
 
-        # TODO: set grounded fake
-        # self.grounded_fake = generate_fake()
+        self.src = input['src'].to(self.device)  # get image data
+        self.tgt = input['tgt'].to(self.device)
+
+        # TODO: set grounded fake, create something in dataloader
+        self.grounded_fake = networks.composite_image(self.src, self.tgt)
 
 
 
     def forward(self):
         """Run forward pass. This will be called by both functions <optimize_parameters> and <test>."""
 
+        # TODO: in the dataloader, we want to have a grounded_fake,
+
 
         # generate output image given the input batch
-        self.g_mask = self.netG(self.batch)
+        self.g_mask = self.netG(self.src)
 
-        # TODO: change this to src and tgt image, must be changed in dataloader
-        self.composite = networks.composite_image(self.batch, self.batch, self.g_mask)
+        # create the composite mask from src and tgt images, and predicted mask
+        self.composite = networks.composite_image(self.src, self.tgt, self.g_mask)
 
-        breakpoint()
+        # TODO: is this sound to create anti shortcut?
+        # apply the masks on different source images, should be labeled false
+        # we reverse the src images over the batch dimension
+        self.anti_shortcut = networks.composite_image(torch.flip(self.src, [0, 1]), self.tgt, self.g_mask)
+
 
 
 
@@ -121,24 +129,24 @@ class CopyPasteGANModel(BaseModel):
         self.loss_G_antishortcut = 0
 
         # add up components and compute gradients
-        self.loss_G = self.loss_G_fake + self.loss_G_antishortcut
+        self.loss_G = self.loss_G_composite + self.loss_G_antishortcut
         self.loss_G.backward()
 
     def backward_D(self):
         """Calculate losses, gradients, and update network weights; called in every training iteration"""
-        # caculate the intermediate results if necessary; here self.fake has been computed during function <forward>
-        # calculate loss given the input and intermediate results
-        # self.loss_D_real = self.criterionGAN(self.fake, self.data_B)
+        # caculate the intermediate results if necessary; here self.composite has been computed during function <forward>
 
-        pred_real = self.netD(self.data_B)
+
+        pred_real = self.netD(self.tgt) # can also be source
         pred_fake = self.netD(self.composite)
-        # TODO: get prediction on grounded fake
-        # pred_groundedfake = self.netD(self.grounded_fake)
+        pred_groundedfake = self.netD(self.grounded_fake)
 
-        self.loss_D_real = self.criterionGAN(self.data_B, True)
-        self.loss_D_fake = self.criterionGAN(self.composite, False)
-        self.loss_D_groundedfake = self.criterionGAN(self.grounded_fake, False)
-        # self.loss_AUX = self.criterionMask()
+        self.loss_D_real = self.criterionGAN(pred_real, True)
+        self.loss_D_fake = self.criterionGAN(pred_fake, False)
+        self.loss_D_groundedfake = self.criterionGAN(pred_groundedfake, False)
+
+        # TODO: create the auxiliary loss
+        self.loss_AUX = 0 # self.criterionMask()
 
         self.loss_D = self.loss_D_real + self.loss_D_fake + loss_D_groundedfake + self.opt.lambda_aux * self.loss_AUX
 
