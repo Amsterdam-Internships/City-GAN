0a274117320bfd3fb886233fc47542d94cbea5a9
Tom Lotze <tom.lotze@student.auc.nl>
2020-12-01 09:48:20 +0100

    two random images are sampled from the data

--- data/double_dataset.py
+++ data/double_dataset.py
@@ -2,6 +2,7 @@ import os
 from data.base_dataset import BaseDataset, get_params, get_transform
 from data.image_folder import make_dataset
 from PIL import Image
+import random
 
 
 class DoubleDataset(BaseDataset):
@@ -18,11 +19,19 @@ class DoubleDataset(BaseDataset):
             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
         BaseDataset.__init__(self, opt)
-        self.dir_AB = os.path.join(opt.dataroot, opt.phase)  # get the image directory
-        self.AB_paths = sorted(make_dataset(self.dir_AB, opt.max_dataset_size))  # get image paths
-        assert(self.opt.load_size >= self.opt.crop_size)   # crop_size should be smaller than the size of loaded image
+        # get the image directory
+        self.data_dir = os.path.join(opt.dataroot, opt.phase)
+        # get paths to all images
+        self.paths = sorted(make_dataset(self.data_dir, opt.max_dataset_size))
+
+        self.size = self.__len__()
+
+        # crop_size should be smaller than the size of loaded image
+        assert(self.opt.load_size >= self.opt.crop_size)
+
         self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
         self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
+
 
     def __getitem__(self, index):
         """Return a data point and its metadata information.
@@ -37,13 +46,11 @@ class DoubleDataset(BaseDataset):
             B_paths (str) - - image paths (same as A_paths)
         """
         # read a image given a random integer index
-        AB_path = self.AB_paths[index]
-        AB = Image.open(AB_path).convert('RGB')
-        # split AB image into A and B
-        w, h = AB.size
-        w2 = int(w / 2)
-        A = AB.crop((0, 0, w2, h))
-        B = AB.crop((w2, 0, w, h))
+        pathA = self.paths[index]
+        indexB = (index + random.randint(1, self.size-1)) % self.size
+        pathB = self.paths[indexB]
+        A = Image.open(pathA).convert('RGB')
+        B = Image.open(pathB).convert('RGB')
 
         # apply the same transform to both A and B
         transform_params = get_params(self.opt, A.size)
@@ -53,8 +60,8 @@ class DoubleDataset(BaseDataset):
         A = A_transform(A)
         B = B_transform(B)
 
-        return {'src': A, 'tgt': B, 'A_paths': AB_path, 'B_paths': AB_path}
+        return {'src': A, 'tgt': B, 'A_paths': pathA, 'B_paths': pathB}
 
     def __len__(self):
         """Return the total number of images in the dataset."""
-        return len(self.AB_paths)
+        return len(self.paths)
