60ed528a19365b7dbc2047a3c25b3acdd05c257e
Tom Lotze <tom.lotze@student.auc.nl>
2020-12-02 12:43:32 +0100

    script to run on Lisa

---
+++ jobscripts/train.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Set job requirements
+#SBATCH -n 16
+#SBATCH -t 59:00
+#SBATCH -p gpu_short
+
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=tom.lotze@gmail.com
+
+#Loading modules
+module load python
+
+
+
+#Create output directory on scratch
+mkdir "$TMPDIR"/datasets
+mkdir "$TMPDIR"/datasets/CLEVR_default
+
+#Copy input file to scratch
+cp -r $HOME/City-GAN/datasets/CLEVR_default/images "$TMPDIR"/datasets/CLEVR_default/
+
+#Execute a Python program located in $HOME, that takes an input file and output directory as arguments.
+python $HOME/train.py --model copypasteGAN --dataroot "$TMPDIR"/datasets/CLEVR_default/images --batch_size 80 --n_epochs 3 --save_epoch_freq 1
+
+
+#Copy output directory from scratch to home
+
+mkdir -p $HOME/City-GAN/checkpoints/test_run
+
+
+# copy checkpoints to home directory
+cp -r "$TMPDIR"/checkpoints $HOME/City-GAN/checkpoints/test_run
