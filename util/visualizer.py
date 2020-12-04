11f289a3401a55260d6a48d8bb99686ac38a2e94
Tom Lotze <tom.lotze@student.auc.nl>
2020-12-04 13:09:52 +0100

    dbugging

--- util/visualizer.py
+++ util/visualizer.py
@@ -74,8 +74,7 @@ class Visualizer():
             self.ncols = opt.display_ncols
             self.vis = visdom.Visdom(server=opt.display_server, port=opt.display_port, env=opt.display_env)
             if not self.vis.check_connection():
-                print("checking connection:", self.vis.check_connection)
-                print("self.vis:", self.vis)
+                print("checking connection:", self.vis.check_connection())
                 self.create_visdom_connections()
 
         if self.use_html:  # create an HTML object at <checkpoints_dir>/web/; images will be saved under <checkpoints_dir>/web/images/
