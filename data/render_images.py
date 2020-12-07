7c6bd3dfdfb15ca0ba517de639e8b61ebf74c527
Tom Lotze <tom.lotze@student.auc.nl>
2020-12-07 12:50:13 +0100

    moved utils inside render_images

--- data/render_images.py
+++ data/render_images.py
@@ -28,18 +28,18 @@ try:
   from mathutils import Vector
 except ImportError as e:
   INSIDE_BLENDER = False
-if INSIDE_BLENDER:
-  try:
-    import utils
-  except ImportError as e:
-    print("\nERROR")
-    print("Running render_images.py from Blender and cannot import utils.py.")
-    print("You may need to add a .pth file to the site-packages of Blender's")
-    print("bundled python with a command like this:\n")
-    print("echo $PWD >> $BLENDER/$VERSION/python/lib/python3.5/site-packages/clevr.pth")
-    print("\nWhere $BLENDER is the directory where Blender is installed, and")
-    print("$VERSION is your Blender version (such as 2.78).")
-    sys.exit(1)
+# if INSIDE_BLENDER:
+#   try:
+#     import utils
+#   except ImportError as e:
+#     print("\nERROR")
+#     print("Running render_images.py from Blender and cannot import utils.py.")
+#     print("You may need to add a .pth file to the site-packages of Blender's")
+#     print("bundled python with a command like this:\n")
+#     print("echo $PWD >> $BLENDER/$VERSION/python/lib/python3.5/site-packages/clevr.pth")
+#     print("\nWhere $BLENDER is the directory where Blender is installed, and")
+#     print("$VERSION is your Blender version (such as 2.78).")
+#     sys.exit(1)
 
 parser = argparse.ArgumentParser()
 
@@ -220,7 +220,7 @@ def render_scene(args,
   bpy.ops.wm.open_mainfile(filepath=args.base_scene_blendfile)
 
   # Load materials
-  utils.load_materials(args.material_dir)
+  load_materials(args.material_dir)
 
   # Set render arguments so we can get pixel coordinates later.
   # We use functionality specific to the CYCLES renderer so BLENDER_RENDER
@@ -285,7 +285,7 @@ def render_scene(args,
 
   # Delete the plane; we only used it for normals anyway. The base scene file
   # contains the actual ground plane.
-  utils.delete_object(plane)
+  delete_object(plane)
 
   # Save all six axis-aligned directions in the scene struct
   scene_struct['directions']['behind'] = tuple(plane_behind)
@@ -310,7 +310,7 @@ def render_scene(args,
   objects, blender_objects = add_random_objects(scene_struct, num_objects, args, camera)
 
 
-  ##### added by me, for random colors
+  ##### added by me, for random colors #############
 
   bg_mat = bpy.data.materials.new(name='bg_mat')
   bg_mat.diffuse_color = random.choice([
@@ -341,7 +341,7 @@ def render_scene(args,
   bpy.context.scene.objects.active = bpy.data.objects['Ground']
   bpy.context.object.active_material = bg_mat
 
-  ######## end added by me
+  ######## end added by me #################
 
   # Render the scene and dump the scene data structure
   scene_struct['objects'] = objects
@@ -398,7 +398,7 @@ def add_random_objects(scene_struct, num_objects, args, camera):
       num_tries += 1
       if num_tries > args.max_retries:
         for obj in blender_objects:
-          utils.delete_object(obj)
+          delete_object(obj)
         return add_random_objects(scene_struct, num_objects, args, camera)
       x = random.uniform(-3, 3)
       y = random.uniform(-3, 3)
@@ -445,17 +445,17 @@ def add_random_objects(scene_struct, num_objects, args, camera):
     theta = 360.0 * random.random()
 
     # Actually add the object to the scene
-    utils.add_object(args.shape_dir, obj_name, r, (x, y), theta=theta)
+    add_object(args.shape_dir, obj_name, r, (x, y), theta=theta)
     obj = bpy.context.object
     blender_objects.append(obj)
     positions.append((x, y, r))
 
     # Attach a random material
     mat_name, mat_name_out = random.choice(material_mapping)
-    utils.add_material(mat_name, Color=rgba)
+    add_material(mat_name, Color=rgba)
 
     # Record data about the object in the scene data structure
-    pixel_coords = utils.get_camera_coords(camera, obj.location)
+    pixel_coords = get_camera_coords(camera, obj.location)
     objects.append({
       'shape': obj_name_out,
       'size': size_name,
@@ -473,7 +473,7 @@ def add_random_objects(scene_struct, num_objects, args, camera):
     # objects from the scene and place them all again.
     print('Some objects are occluded; replacing objects')
     for obj in blender_objects:
-      utils.delete_object(obj)
+      delete_object(obj)
     return add_random_objects(scene_struct, num_objects, args, camera)
 
   return objects, blender_objects
@@ -552,10 +552,10 @@ def render_shadeless(blender_objects, path='flat.png'):
   render_args.use_antialiasing = False
 
   # Move the lights and ground to layer 2 so they don't render
-  utils.set_layer(bpy.data.objects['Lamp_Key'], 2)
-  utils.set_layer(bpy.data.objects['Lamp_Fill'], 2)
-  utils.set_layer(bpy.data.objects['Lamp_Back'], 2)
-  utils.set_layer(bpy.data.objects['Ground'], 2)
+  set_layer(bpy.data.objects['Lamp_Key'], 2)
+  set_layer(bpy.data.objects['Lamp_Fill'], 2)
+  set_layer(bpy.data.objects['Lamp_Back'], 2)
+  set_layer(bpy.data.objects['Ground'], 2)
 
   # Add random shadeless materials to all objects
   object_colors = set()
@@ -581,10 +581,10 @@ def render_shadeless(blender_objects, path='flat.png'):
     obj.data.materials[0] = mat
 
   # Move the lights and ground back to layer 0
-  utils.set_layer(bpy.data.objects['Lamp_Key'], 0)
-  utils.set_layer(bpy.data.objects['Lamp_Fill'], 0)
-  utils.set_layer(bpy.data.objects['Lamp_Back'], 0)
-  utils.set_layer(bpy.data.objects['Ground'], 0)
+  set_layer(bpy.data.objects['Lamp_Key'], 0)
+  set_layer(bpy.data.objects['Lamp_Fill'], 0)
+  set_layer(bpy.data.objects['Lamp_Back'], 0)
+  set_layer(bpy.data.objects['Ground'], 0)
 
   # Set the render settings back to what they were
   render_args.filepath = old_filepath
@@ -594,10 +594,175 @@ def render_shadeless(blender_objects, path='flat.png'):
   return object_colors
 
 
+
+
+########################### HELPER FUNCTIONS ##################################
+
+
+def extract_args(input_argv=None):
+  """
+  Pull out command-line arguments after "--". Blender ignores command-line flags
+  after --, so this lets us forward command line arguments from the blender
+  invocation to our own script.
+  """
+  if input_argv is None:
+    input_argv = sys.argv
+  output_argv = []
+  if '--' in input_argv:
+    idx = input_argv.index('--')
+    output_argv = input_argv[(idx + 1):]
+  return output_argv
+
+
+def parse_args(parser, argv=None):
+  return parser.parse_args(extract_args(argv))
+
+
+# I wonder if there's a better way to do this?
+def delete_object(obj):
+  """ Delete a specified blender object """
+  for o in bpy.data.objects:
+    o.select = False
+  obj.select = True
+  bpy.ops.object.delete()
+
+
+def get_camera_coords(cam, pos):
+  """
+  For a specified point, get both the 3D coordinates and 2D pixel-space
+  coordinates of the point from the perspective of the camera.
+
+  Inputs:
+  - cam: Camera object
+  - pos: Vector giving 3D world-space position
+
+  Returns a tuple of:
+  - (px, py, pz): px and py give 2D image-space coordinates; pz gives depth
+    in the range [-1, 1]
+  """
+  scene = bpy.context.scene
+  x, y, z = bpy_extras.object_world_to_camera_view(scene, cam, pos)
+  scale = scene.render.resolution_percentage / 100.0
+  w = int(scale * scene.render.resolution_x)
+  h = int(scale * scene.render.resolution_y)
+  px = int(round(x * w))
+  py = int(round(h - y * h))
+  return (px, py, z)
+
+
+def set_layer(obj, layer_idx):
+  """ Move an object to a particular layer """
+  # Set the target layer to True first because an object must always be on
+  # at least one layer.
+  obj.layers[layer_idx] = True
+  for i in range(len(obj.layers)):
+    obj.layers[i] = (i == layer_idx)
+
+
+def add_object(object_dir, name, scale, loc, theta=0):
+  """
+  Load an object from a file. We assume that in the directory object_dir, there
+  is a file named "$name.blend" which contains a single object named "$name"
+  that has unit size and is centered at the origin.
+
+  - scale: scalar giving the size that the object should be in the scene
+  - loc: tuple (x, y) giving the coordinates on the ground plane where the
+    object should be placed.
+  """
+  # First figure out how many of this object are already in the scene so we can
+  # give the new object a unique name
+  count = 0
+  for obj in bpy.data.objects:
+    if obj.name.startswith(name):
+      count += 1
+
+  filename = os.path.join(object_dir, '%s.blend' % name, 'Object', name)
+  bpy.ops.wm.append(filename=filename)
+
+  # Give it a new name to avoid conflicts
+  new_name = '%s_%d' % (name, count)
+  bpy.data.objects[name].name = new_name
+
+  # Set the new object as active, then rotate, scale, and translate it
+  x, y = loc
+  bpy.context.scene.objects.active = bpy.data.objects[new_name]
+  bpy.context.object.rotation_euler[2] = theta
+  bpy.ops.transform.resize(value=(scale, scale, scale))
+  bpy.ops.transform.translate(value=(x, y, scale))
+
+
+def load_materials(material_dir):
+  """
+  Load materials from a directory. We assume that the directory contains .blend
+  files with one material each. The file X.blend has a single NodeTree item named
+  X; this NodeTree item must have a "Color" input that accepts an RGBA value.
+  """
+  for fn in os.listdir(material_dir):
+    if not fn.endswith('.blend'): continue
+    name = os.path.splitext(fn)[0]
+    filepath = os.path.join(material_dir, fn, 'NodeTree', name)
+    bpy.ops.wm.append(filename=filepath)
+
+
+def add_material(name, **properties):
+  """
+  Create a new material and assign it to the active object. "name" should be the
+  name of a material that has been previously loaded using load_materials.
+  """
+  # Figure out how many materials are already in the scene
+  mat_count = len(bpy.data.materials)
+
+  # Create a new material; it is not attached to anything and
+  # it will be called "Material"
+  bpy.ops.material.new()
+
+  # Get a reference to the material we just created and rename it;
+  # then the next time we make a new material it will still be called
+  # "Material" and we will still be able to look it up by name
+  mat = bpy.data.materials['Material']
+  mat.name = 'Material_%d' % mat_count
+
+  # Attach the new material to the active object
+  # Make sure it doesn't already have materials
+  obj = bpy.context.active_object
+  assert len(obj.data.materials) == 0
+  obj.data.materials.append(mat)
+
+  # Find the output node of the new material
+  output_node = None
+  for n in mat.node_tree.nodes:
+    if n.name == 'Material Output':
+      output_node = n
+      break
+
+  # Add a new GroupNode to the node tree of the active material,
+  # and copy the node tree from the preloaded node group to the
+  # new group node. This copying seems to happen by-value, so
+  # we can create multiple materials of the same type without them
+  # clobbering each other
+  group_node = mat.node_tree.nodes.new('ShaderNodeGroup')
+  group_node.node_tree = bpy.data.node_groups[name]
+
+  # Find and set the "Color" input of the new group node
+  for inp in group_node.inputs:
+    if inp.name in properties:
+      inp.default_value = properties[inp.name]
+
+  # Wire the output of the new group node to the input of
+  # the MaterialOutput node
+  mat.node_tree.links.new(
+      group_node.outputs['Shader'],
+      output_node.inputs['Surface'],
+  )
+
+
+########################### HELPER FUNCTIONS ##################################
+
+
 if __name__ == '__main__':
   if INSIDE_BLENDER:
     # Run normally
-    argv = utils.extract_args()
+    argv = extract_args()
     args = parser.parse_args(argv)
     main(args)
   elif '--help' in sys.argv or '-h' in sys.argv:
