992471c24686d073d2b38d44e26db27bc5fc4538
Tom-Lotze <tom.lotze@student.auc.nl>
2020-12-16 20:41:12 +0100

    clean working dir

--- jobscripts/color_train.sh
+++ jobscripts/color_train.sh
@@ -26,8 +26,8 @@ cp -r $HOME/City-GAN/datasets/CLEVR_colorized/images "$TMPDIR"/datasets/CLEVR_co
 python $HOME/City-GAN/train.py --model copypasteGAN \
     --dataroot "$TMPDIR"/datasets/CLEVR_colorized/images\
     --batch_size 50\
-    --n_epochs 12\
-    --n_epochs_decay 5\
+    --n_epochs 15\
+    --n_epochs_decay 10\
     --save_epoch_freq 10\
     --checkpoints_dir "$TMPDIR"/checkpoints\
     --print_freq 1000\
@@ -35,7 +35,7 @@ python $HOME/City-GAN/train.py --model copypasteGAN \
     --display_freq 5000\
     --verbose\
     --sigma_blur 1 \
-    --D_headstart 80000\
+    --D_headstart 40000\
 
 # copy checkpoints to home directory
 mkdir -p $HOME/City-GAN/checkpoints/run"${run}"
