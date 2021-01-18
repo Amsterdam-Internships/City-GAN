486515a28c5100cd14a5e4371a8c8752a10d1369
Tom Lotze <tom.lotze@student.auc.nl>
2021-01-18 16:28:21 +0100

    rotation must be int

--- models/copypasteGAN_model.py
+++ models/copypasteGAN_model.py
@@ -219,9 +219,10 @@ class CopyPasteGANModel(BaseModel):
 
         if self.opt.rotate_img:
             import torchvision.transforms.functional as TF
-            angle_src = np.random.choice([0, 90, 180, 270])
+            angle_src = int(np.random.choice([0, 90, 180, 270]))
+            breakpoint()
             self.src = TF.rotate(self.src, angle_src)
-            angle_tgt = np.random.choice([0, 90, 180, 270])
+            angle_tgt = int(np.random.choice([0, 90, 180, 270]))
             self.tgt = TF.rotate(self.tgt, angle_tgt)
 
         # create a grounded fake, the function samples a random polygon mask
