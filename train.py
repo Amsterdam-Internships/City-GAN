441eca61c298e8308ea5c6dea389534a1a6d6f04
Tom Lotze <tom.lotze@student.auc.nl>
2021-01-25 11:26:04 +0100

    printing blur filter from dataparallel object

--- train.py
+++ train.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
 
         print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
 
-        print(f"Gaussian filter weight at end of epoch {epoch}: {model.netD.blur_filter.weight}")
+        print(f"Gaussian filter weight at end of epoch {epoch}: {model.netD.module.blur_filter.weight}")
 
     model.save_networks('latest')
     print("Finished training, model is saved")
