572fc47ecc6e4390cc3333ffdeb06da86557670e
Tom Lotze <tom.lotze@student.auc.nl>
2021-02-16 15:29:19 +0100

    skeleton for trainable move model

--- models/move_model.py
+++ models/move_model.py
@@ -54,28 +54,120 @@ class MoveModel(BaseModel):
         - define loss function, visualization images, model names, and optimizers
         """
         BaseModel.__init__(self, opt)  # call the initialization method of BaseModel
-        # specify the training losses you want to print out. The program will call base_model.get_current_losses to plot the losses to the console and save them to the disk.
+
         self.loss_names = []
-        # specify the images you want to save and display. The program will call base_model.get_current_visuals to save and display these images.
-        self.visual_names = ["img", "moved"]
-        # specify the models you want to save to the disk. The program will call base_model.save_networks and base_model.load_networks to save and load networks.
-        # you can use opt.isTrain to specify different behaviors for training and test. For example, some networks will not be used during test, and you don't need to load them.
-        self.model_names = []
-        # define networks; you can use opt.isTrain to specify different behaviors for training and test.
-        # self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, gpu_ids=self.gpu_ids)
+
+        # define variables for plotting and saving
+        self.visual_names = ["tgt", "obj", "composite"]
+
+        # define the convnet that predicts theta
+        self.ConvNet = networks.MoveConvNET()
+
+        self.model_names = ["ConvNet"]
+
         if self.isTrain:  # only defined during training time
-            # define your loss functions. You can use losses provided by torch.nn such as torch.nn.L1Loss.
-            # We also provide a GANLoss class "networks.GANLoss". self.criterionGAN = networks.GANLoss().to(self.device)
-            self.criterionLoss = torch.nn.L1Loss()
-            # define and initialize optimizers. You can define one optimizer for each network.
-            # If two networks are updated at the same time, you can use itertools.chain to group them. See cycle_gan_model.py for an example.
-            # self.optimizer = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-            # self.optimizers = [self.optimizer]
+            self.netD = networks.define_D(opt.input_nc, ndf, opt.netD)
+            self.model_names.append("netD")
+
+            self.criterionGAN = networks.GANLoss("vanilla")
+
 
         # Our program will automatically call <model.setup> to define schedulers, load networks, and print networks
 
 
-    def inference(self, data, obj_idx = -1, type_='random'):
+
+
+
+
+
+    def set_input(self, input):
+        """
+        What do we need:
+
+            - centered object + object mask (from source)
+            - some real image (e.g. source)
+            - target image
+
+        """
+        self.img = input['img']
+        self.nr_masks = int(input["nr_masks"])
+
+        self.tgt = None
+        self.obj = None
+        self.src = None
+        self.obj_mask = None
+
+
+        # assign the masks to the model
+        for i in range(self.nr_masks):
+            setattr(self, f"mask_{i}", input[f"mask{i}"])
+
+
+    def forward(self):
+        """
+        what needs to be done:
+            - target image and centered object are fed to convnet (initialized in the init)
+            - theta parameters are the output
+            - affine transformation on the object and the object mask
+            - the transformed object and object masks are composited --> output img
+        """
+        # concatenate the target and object on channel dimension
+        tgt_obj_concat = torch.cat(self.tgt, self.obj, 1)
+
+        # compute theta using the convolutional network
+        self.theta = self.ConvNet(tgt_obj_concat)
+
+        # make sure theta is scaled: preventing object from moving outside img
+
+        # use theta to transform the object and the mask
+        # is affine the correct function? perhaps we should use affine_grid
+        self.transf_obj = affine(self.obj, angle=, scale=, shear=)
+        self.transf_obj_mask = affine(self.obj_mask, angle=, scale=, shear=)
+
+
+        # composite the moved object with the background from the target
+        self.composite, _ = networks.composite_image(self.transf_obj, self.tgt, self.transf_obj_mask)
+
+        # get the prediction on the fake image
+        self.pred_fake = self.netD(self.composite)
+
+        # get the prediction on the real image
+        self.pred_real = self.netD(self.src)
+
+
+
+
+    def backward(self):
+        """
+        todo here
+            - compute the losses (GAN loss real fake and possibly more)
+            - backward over the loss for D
+            - backward over the loss to update the ConvNet
+            - perhaps use AMP scaler again?
+
+        """
+
+        pass
+
+
+    def optimize_parameters(self, data, overall_batch):
+        """Update network weights; it will be called in every training iteration.
+        """
+        # run the forward pass
+        self.forward()
+
+
+        self.optimizer.zero_grad()   # clear network G's existing gradients
+        self.backward()              # calculate gradients for network G
+        self.optimizer.step()        # update gradients for network G
+
+
+
+
+
+
+
+    def baseline(self, data, obj_idx = -1, type_='random'):
 
         assert type_ in {"random", "scanline"}, f"Type {type_} not recognized, choose from \"random\" or \"scanline\""
 
@@ -94,7 +186,7 @@ class MoveModel(BaseModel):
         obj_mask = getattr(self, f"mask_{obj_idx}")
         obj_mask_binary = (obj_mask > 0).int()
         obj_width = int(torch.max(torch.sum(obj_mask>0, axis=2)))
-        obj_height = int(torch.max(torch.sum(obj_mask>0, axis=1)))
+        obj_height = int(torch.max(torch.sum(obj_mask>0, axis=3)))
 
         obj_size_approx = obj_width * obj_height
 
@@ -106,11 +198,11 @@ class MoveModel(BaseModel):
 
         # x translation is always used
         # x_translation = np.random.randint(obj_width, img_width - obj_width)
-        x_translation = obj_width
-
+        x_translation = (1 if np.random.random() < 0.5 else -1) * obj_width
+        # x_translation = np.random.normal(obj_width, 2*obj_width)
 
         if type_=="random":
-            y_translation = -obj_height
+            y_translation = (1 if np.random.random() < 0.5 else -1) * obj_height
 
         elif type_=="scanline":
             # we want to obtain x_min and x_max of the object, and the width of the object (x_max-x_min). Then we move the object to the right with at least width, and maximally (img_width - width), and modulo the transformation with img_width
@@ -120,7 +212,6 @@ class MoveModel(BaseModel):
                 return None, None
 
             y_translation = 0
-
 
         moved_obj = affine(obj, 0, [x_translation, y_translation], 1, 0)
         new_background = 1 - (moved_obj != 0).int()
@@ -133,7 +224,7 @@ class MoveModel(BaseModel):
         ax2.imshow(util.tensor2im(obj), origin="upper")
         ax2.set_title("object")
         ax3.imshow(util.tensor2im(moved_obj), origin="upper")
-        ax3.set_title("moved_obj")
+        ax3.set_title(f"moved_obj ({x_translation}, {y_translation})")
         ax4.imshow(util.tensor2im(self.moved), origin="upper")
         ax4.set_title("result")
         plt.tight_layout()
@@ -141,44 +232,3 @@ class MoveModel(BaseModel):
 
 
         return self.img, self.moved
-
-
-
-
-    def set_input(self, input):
-        """Unpack input data from the dataloader and perform necessary pre-processing steps.
-
-        Parameters:
-            input: a dictionary that contains the data itself and its metadata information.
-        """
-        self.img = input['img']
-        self.nr_masks = int(input["nr_masks"])
-
-        # concatenate the masks into one
-        #
-        for i in range(self.nr_masks):
-            setattr(self, f"mask_{i}", input[f"mask{i}"])
-        # self.masks = torch.cat([input[f"mask{i}"] for i
-            # in range(self.nr_masks)], 1)
-
-
-
-    def forward(self):
-        """Run forward pass. This will be called by both functions <optimize_parameters> and <test>."""
-        self.output = self.netG(self.data_A)  # generate output image given the input data_A
-
-    def backward(self):
-        """Calculate losses, gradients, and update network weights; called in every training iteration"""
-        # caculate the intermediate results if necessary; here self.output has been computed during function <forward>
-        # calculate loss given the input and intermediate results
-        self.loss_G = self.criterionLoss(self.output, self.data_B) * self.opt.lambda_regression
-        self.loss_G.backward()       # calculate gradients of network G w.r.t. loss_G
-
-    def optimize_parameters(self, data, overall_batch):
-        """Update network weights; it will be called in every training iteration."""
-        self.forward()               # first call forward to calculate intermediate results
-
-        breakpoint()
-        self.optimizer.zero_grad()   # clear network G's existing gradients
-        self.backward()              # calculate gradients for network G
-        self.optimizer.step()        # update gradients for network G
