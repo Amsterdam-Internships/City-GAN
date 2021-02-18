d72c4b75914a36c301cd16433c267cc5c42e6c45
Tom Lotze <tom.lotze@student.auc.nl>
2021-02-18 17:27:53 +0100

    dataset picks random mask if it is large enough

--- data/room_dataset.py
+++ data/room_dataset.py
@@ -1,8 +1,10 @@
 from data.base_dataset import BaseDataset, get_transform
 from data.image_folder import make_dataset
 from PIL import Image
+import torch
 import os
 import glob
+import random
 
 
 class RoomDataset(BaseDataset):
@@ -19,13 +21,14 @@ class RoomDataset(BaseDataset):
         """
         BaseDataset.__init__(self, opt)
 
-        self.data_dir = os.path.join(opt.dataroot, opt.phase, "images_six_objects")
-        input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
+        # images can be found directly in the phase folder
+        self.data_dir = os.path.join(opt.dataroot, opt.phase)
 
         # check if the transform is the same if used multiple times (the random components)
-        self.transform_img = get_transform(opt, grayscale=(input_nc == 1))
+        self.transform_img = get_transform(opt, grayscale=False)
         self.transform_mask = get_transform(opt, grayscale=True)
 
+        self.min_obj_surface = opt.min_obj_surface
 
         # incorporate the max length in here?
         self.length = min(len(glob.glob1(self.data_dir,"*img*")), opt.max_dataset_size)
@@ -41,25 +44,54 @@ class RoomDataset(BaseDataset):
         Returns a dictionary that contains A and A_paths
             img (tensor) - - an image in one domain
             masks (tensor) - - al the masks concatenated
+
+
+        This function should return:
+        - tgt and source image (normal images)
+        - (centered) object from source
+        - corresponding centered mask from obj
+
         """
 
-        img_path = os.path.join(self.data_dir, f"{index}_img.jpg")
-
-        mask_paths = sorted(glob.glob(os.path.join(self.data_dir, f"{index}_mask_*.jpg")))
-
-        img = Image.open(img_path).convert('RGB')
-        img = self.transform_img(img)
-
         out_dict = dict()
-        out_dict['img'] = img
 
+        # extract source image based on index, and random target image
+        img_path_src = os.path.join(self.data_dir, f"{index}_img.jpg")
+        index_tgt =(index + random.randint(1, self.length-1)) % self.length
+        img_path_tgt = os.path.join(self.data_dir, f"{index_tgt}_img.jpg")
 
-        for i, p in enumerate(mask_paths):
-            mask = Image.open(p).convert("1")
-            mask = self.transform_mask(mask)
-            out_dict[f"mask{i}"] = mask
+        # open and convert images
+        img_src = Image.open(img_path_src).convert('RGB')
+        img_tgt = Image.open(img_path_tgt).convert('RGB')
+        out_dict['src'] = self.transform_img(img_src)
+        out_dict['tgt'] = self.transform_img(img_tgt)
 
-        out_dict["nr_masks"] = i+1
+        # extract all src masks
+        mask_paths = sorted(glob.glob(os.path.join(self.data_dir, f"{index}_mask_*.jpg")))[4:]
+
+        random.shuffle(mask_paths)
+
+        # find suitable mask
+
+        for p in mask_paths:
+            mask = self.transform_mask(Image.open(p).convert("1"))
+            mask_binary = (mask > 0).int()
+            surface = mask_binary.sum().item()
+
+            if surface > self.opt.min_obj_surface:
+                out_dict["mask"] = mask
+                return out_dict
+
+        out_dict["mask"] = None
+
+        # # concatenate version:
+        # out_dict["src_masks"] = torch.cat([self.transform_mask(Image.open(p).convert("1")) for p in mask_paths[4:]])
+
+        # we skip the first 4 paths (floor, sky, walls)
+        # for i, p in enumerate(mask_paths[4:]):
+        #     mask = Image.open(p).convert("1")
+        #     mask = self.transform_mask(mask)
+        #     out_dict[f"src_mask{i}"] = mask
 
         return out_dict
 
