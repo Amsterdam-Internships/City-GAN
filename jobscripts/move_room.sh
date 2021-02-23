9bc7c3b8d13f5116a49a79b53c672b9c4c3a700c
Tom-Lotze <tom.lotze@student.auc.nl>
2021-02-23 20:16:31 +0100

    include validation set

--- jobscripts/move_room.sh
+++ jobscripts/move_room.sh
@@ -37,7 +37,7 @@ mkdir "$TMPDIR"/datasets/ROOM/images/val
 # move the tar file in home to scratch
 cp $HOME/City-GAN/datasets/ROOM/images/train/10k_train.tar.gz "$TMPDIR"/datasets/ROOM/images/train/
 
-ls  "$TMPDIR"/datasets/ROOM/images/train | head -n 5
+# ls  "$TMPDIR"/datasets/ROOM/images/train | head -n 5
 
 echo "Tar file moved to scratch"
 now=$(date +"%T")
@@ -47,7 +47,7 @@ echo
 # unpack the tar on scratch
 tar -zxf "$TMPDIR"/datasets/ROOM/images/train/10k_train.tar.gz --strip-components 1 --directory "$TMPDIR"/datasets/ROOM/images/train/
 
-ls "$TMPDIR/datasets/ROOM/images/train"
+ls "$TMPDIR/datasets/ROOM/images/train" | head -n 5
 
 echo "Tar file extracted on scratch"
 now=$(date +"%T")
@@ -57,32 +57,37 @@ echo
 # old way of copying
 # cp -r $HOME/City-GAN/datasets/ROOM/images/train/images_train/111**.jpg "$TMPDIR"/datasets/ROOM/images/val/
 
-# cp cp $HOME/City-GAN/datasets/ROOM/images/val/1k_val.tar.gz "$TMPDIR"/datasets/ROOM/images/val/
+cp $HOME/City-GAN/datasets/ROOM/images/val/1k_val.tar.gz "$TMPDIR"/datasets/ROOM/images/val/
 
-# echo "Validation tar copied to scratch"
-# now=$(date +"%T")
-# echo "Current time : $now"
-# echo
+echo "Validation tar copied to scratch"
+now=$(date +"%T")
+echo "Current time : $now"
+echo
 
-# tar -zxf "$TMPDIR"/datasets/ROOM/images/val/1k_val.tar.gz --strip-components 1 --directory "$TMPDIR"/datasets/ROOM/images/val/
+tar -zxf "$TMPDIR"/datasets/ROOM/images/val/1k_val.tar.gz --strip-components 1 --directory "$TMPDIR"/datasets/ROOM/images/val/
 
 
-# echo "validation tar extracted on scratch"
-# now=$(date +"%T")
-# echo "Current time : $now"
-# echo
+echo "validation tar extracted on scratch"
+now=$(date +"%T")
+echo "Current time : $now"
+echo
 
 # execute training script
 python $HOME/City-GAN/train_move.py --model move \
     --dataroot "$TMPDIR"/datasets/ROOM/images/\
     --name Move\
-    --max_dataset_size 10\
-    --batch_size 5\
-    --epoch 20\
+    --max_dataset_size 10000\
+    --batch_size 64\
+    --n_epochs 5\
+    --n_epochs_decay 2\
     --checkpoints_dir "$TMPDIR"/checkpoints\
     --display_id 0\
-    --num_threads 1\
-    --min_obj_surface 55
+    --num_threads 4\
+    --min_obj_surface 80\
+    --print_freq 20\
+    --display_freq 100\
+    --update_html 100\
+    
 
 
 # copy checkpoints to home directory
