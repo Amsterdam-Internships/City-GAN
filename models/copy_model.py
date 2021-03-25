63d6aeced314c5bcbcbe98c3352a5f4fca87e444
Tom Lotze <tom.lotze@student.auc.nl>
2021-03-25 08:59:03 +0100

    code for run 78, no headstart

--- models/copy_model.py
+++ models/copy_model.py
@@ -126,7 +126,7 @@ class CopyModel(BaseModel):
             help="If specified, the data will be flipped vertically",
         )
         parser.add_argument(
-            "--no_alternate",
+            "--sequential",
             action="store_true",
             help="If specified, G and D will not be trained in alternating\
                 fashion, but sequentially (for val_freq batches each)",
@@ -496,7 +496,7 @@ class CopyModel(BaseModel):
 
         # determine if G can be trained
         # G and D are trained sequentially for eval_freq batches
-        alt_cond = self.opt.no_alternate and ((total_batches // 100) % 2 == 0)
+        alt_cond = self.opt.sequential and ((total_batches // 100) % 2 == 0)
         batch_right = self.even_batch or alt_cond
 
         if self.headstart_over and self.D_above_thresh and batch_right:
