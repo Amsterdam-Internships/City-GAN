c9c7ee7498e288d3dadad4f6fb651c7ac91e13d1
Tom Lotze <tom.lotze@student.auc.nl>
2021-05-07 10:15:39 +0200

    copy only 10K tar to scratch

--- jobscripts/generate_move_eval_dataset.sh
+++ jobscripts/generate_move_eval_dataset.sh
@@ -18,7 +18,10 @@ run=21
 mkdir "$TMPDIR"/datasets
 
 #Copy data file to scratch
-cp -r $HOME/City-GAN/datasets/ROOM/images "$TMPDIR"/datasets/ROOM/
+cp $HOME/City-GAN/datasets/ROOM/images/train/10k_train.tar.gz "$TMPDIR"/datasets/ROOM/images/train/
+tar -zxf "$TMPDIR"/datasets/ROOM/images/train/10k_train.tar.gz --strip-components 1 --directory "$TMPDIR"/datasets/ROOM/images/train/
+# show first five images
+ls "$TMPDIR/datasets/ROOM/images/train" | head -n 5
 
 # execute training script
 python $HOME/City-GAN/create_move_eval_dataset.py.py --model move \
